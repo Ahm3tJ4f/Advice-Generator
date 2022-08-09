844b33cd67f24d2d9f72a89bf64d4637fe69f2c9
Ahmed Caferov <ahmetleo56@gmail.com>
2022-08-09 20:33:03 +0400

    reorganize project files

--- app.js
+++ app.js
@@ -1,26 +1,25 @@
 const URL = 'https://api.adviceslip.com/advice';
 
 const getAdvice = async () => {
-  const response = await axios.get(URL);
-  const quote = response.data.slip.advice;
-  const id = response.data.slip.id;
+  let response = await axios.get(URL);
+  console.log(response.data);
+  let quote = response.data.slip.advice;
+  let id = response.data.slip.id;
 
   document.querySelector('.advice__quote').innerHTML = quote;
-  document.querySelector('.advice__title').innerHTML = `Advice #${id}`;
-  console.log(quote);
-  console.log(id);
-
-  
+  document.querySelector('.advice__title').innerHTML = `Advice #${id}`;  
 };
 
 const button = document.querySelector('.card__button');
-
 button.addEventListener('click', getAdvice);
 
 button.addEventListener('click', function () {
+
   const dice = document.querySelector('.card__button--dice');
   dice.classList.remove('rotate');
+
   setTimeout(() => {
     dice.classList.add('rotate');
   });
+
 });
